2e0e6e9e12ab8302df5d5fee342665edd0ca8a61
Belinda Vennam <beemarie@us.ibm.com>
2017-10-16 16:16:16 -0500

    run tests

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -1,15 +1,16 @@
 # Build script for Travis-CI.
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-ROOTDIR="$SCRIPTDIR/../../openwhisk"
+WHISKDIR="$SCRIPTDIR/../../openwhisk"
+ROOTDIR="$SCRIPTDIR/../.."
 
-cd $ROOTDIR
+cd $WHISKDIR
 
-cp $ROOTDIR/../tests/src/* $ROOTDIR/tests/src/packages/
+cp $WHISKDIR/../tests/src/* $WHISKDIR/tests/src/packages/
 
 tools/build/scanCode.py .
 
-cd $ROOTDIR/ansible
+cd $WHISKDIR/ansible
 
 ANSIBLE_CMD="ansible-playbook -i environments/travis"
 
@@ -18,32 +19,33 @@ $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
 
-cd $ROOTDIR
+cd $WHISKDIR
 
 ./gradlew distDocker
 
-cd $ROOTDIR/ansible
+cd $WHISKDIR/ansible
 
 $ANSIBLE_CMD openwhisk.yml
 
-cd $ROOTDIR
+cd $WHISKDIR
 
-VCAP_SERVICES_FILE="$(readlink -f $ROOTDIR/../tests/credentials.json)"
+VCAP_SERVICES_FILE="$(readlink -f $WHISKDIR/../tests/credentials.json)"
 
 #update whisk.properties to add tests/credentials.json file to vcap.services.file, which is needed in tests
-WHISKPROPS_FILE="$ROOTDIR/whisk.properties"
+WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
 sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
 cat whisk.properties
 
-WSK_CLI=$ROOTDIR/bin/wsk
-AUTH_KEY=$(cat $ROOTDIR/ansible/files/auth.whisk.system)
-EDGE_HOST=$(grep '^edge.host=' $WHISKPROPS_FILE | cut -d'=' -f2) 
+WSK_CLI=$WHISKDIR/bin/wsk
+AUTH_KEY=$(cat $WHISKDIR/ansible/files/auth.whisk.system)
+EDGE_HOST=$(grep '^edge.host=' $WHISKPROPS_FILE | cut -d'=' -f2)
 
-# Install the package
-source $ROOTDIR/../install.sh $EDGE_HOST $AUTH_KEY $WSK_CLI
+cd ${ROOTDIR}
+TERM=dumb ./gradlew :tests:test
+# # Install the package
+# source $WHISKDIR/../install.sh $EDGE_HOST $AUTH_KEY $WSK_CLI
 
-#Test only the test cases classes in tests/src (Openwhisk dependencies are needed)
-X="./gradlew :tests:test "
-for f in $(ls $ROOTDIR/../tests/src | sed -e 's/\..*$//'); do X="$X --tests \"packages.$f\""; done
-eval $X
-
+# #Test only the test cases classes in tests/src (Openwhisk dependencies are needed)
+# X="./gradlew :tests:test "
+# for f in $(ls $WHISKDIR/../tests/src | sed -e 's/\..*$//'); do X="$X --tests \"packages.$f\""; done
+# eval $X
