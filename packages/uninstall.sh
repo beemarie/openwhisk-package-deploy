5378fb9df73a89a4c277fe322c57b7f468ea014c
Belinda Vennam <beemarie@us.ibm.com>
2017-10-16 11:52:36 -0500

    moving uninstall

--- packages/uninstall.sh
+++ packages/uninstall.sh
@@ -14,6 +14,6 @@ WSK_CLI="$3"
 
 echo Uninstalling Template Package \
 
-$WSK_CLI --apihost $APIHOST action delete -i --auth $AUTH packageTemplate/helloworld
+$WSK_CLI --apihost $APIHOST action delete -i --auth $AUTH deploy2/wskdeploy
 
-$WSK_CLI --apihost $APIHOST package delete -i --auth $AUTH packageTemplate
+$WSK_CLI --apihost $APIHOST package delete -i --auth $AUTH deploy2
